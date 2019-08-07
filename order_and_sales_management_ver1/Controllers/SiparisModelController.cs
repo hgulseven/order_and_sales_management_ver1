3808767e2c374a3e30226f230f719fc1042347b9
hgulseven <hakan@gulseven.com.tr>
2019-08-08 01:37:35 +0300

    Siparis girişi eklendi.

---
+++ order_and_sales_management_ver1/Controllers/SiparisModelController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using order_and_sales_management_ver1.Data;
+using Order_And_Sales_Management_ver1.Models;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
+
+namespace order_and_sales_management_ver1.Controllers
+{
+    public class SiparisModelController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SiparisModelController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult Giris(string productName)
+        {
+            SiparisModel siparis=new SiparisModel();
+            siparis.orderDate = DateTime.Now;
+            ViewData["personelID"] = new SelectList(_context.EmployeesModel, nameof(SiparisModel.orderOwnerEmployeeModel.personelID), nameof(SiparisModel.orderOwnerEmployeeModel.persName));
+            EmployeesModel employees = _context.EmployeesModel.First();
+            siparis.orderLocation = _context.StockLocationModel.FirstOrDefault(e => e.locationID == employees.locationID);
+            if (!String.IsNullOrEmpty(productName))
+            {
+                siparis.products = _context.ProductModels.Where(x => x.ProductName.Contains(productName)).ToList();
+            }
+            else
+            {
+                siparis.products = new List<ProductModel>();
+
+            }
+            return View(siparis);
+        }
+        [HttpPost]
+        public ActionResult Giris(string jsonSiparis, string jsonOrderDetails)
+        {
+            JObject jobjSiparis;
+            JArray jobjOrderDetails;
+            OrderModel siparis=new OrderModel();
+
+            siparis.OrderDetailsModels = new List<OrderDetailsModel>();
+
+            if (ModelState.IsValid)
+            {
+                jobjSiparis = JObject.Parse(jsonSiparis);
+                foreach (KeyValuePair<String, JToken> app in jobjSiparis)
+                {
+                    var appName = app.Key;
+                    var value = (String)app.Value;
+                    switch (appName)
+                    {
+                        case "orderDate":
+                            siparis.orderDate = DateTime.Parse(value);
+                            break;
+                        case "orderOwner_personelID":
+                            siparis.orderOwner_personelID = int.Parse(value);
+                            break;
+                        case "LocationID":
+                            siparis.orderLocationID = int.Parse(value);
+                            break;
+                    }
+                }
+                siparis.recStatus = true;
+                jobjOrderDetails = JArray.Parse(jsonOrderDetails);
+                foreach (JObject jObj in jobjOrderDetails)
+                {
+                    OrderDetailsModel orderDetails = new OrderDetailsModel();
+                    foreach (KeyValuePair<String, JToken> app in jObj)
+                    {
+                        var appName = app.Key;
+                        var value = (String)app.Value;
+                        switch (appName)
+                        {
+                            case "orderLineNo":
+                                orderDetails.orderLineNo = int.Parse(value);
+                                break;
+                            case "productID":
+                                orderDetails.productID = int.Parse(value);
+                                break;
+                            case "productAmount":
+                                orderDetails.productAmount = int.Parse(value);
+                                break;
+                        }
+                    }
+                    orderDetails.recStatus = true;
+                    siparis.OrderDetailsModels.Add(orderDetails);
+                }
+                _context.Add(siparis);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Giris),new { productName = "" });
+        }
+
+        public bool StockItemExists(int prodID,  string lotID)
+        {
+            int locID = 4;
+            return _context.StockItems.Any(e => e.productID == prodID && e.locationID == locID && e.productionLotID == lotID);
+        }
+
+        public StockLocationModel getLocation(int employeeID)
+        {
+            EmployeesModel employee;
+            StockLocationModel stockLocationModel;
+
+            employee=_context.EmployeesModel.FirstOrDefault(e => e.personelID == employeeID);
+            stockLocationModel = _context.StockLocationModel.FirstOrDefault(e => e.locationID == employee.locationID);
+            return stockLocationModel;
+        }
+
+        public SiparisModel getProducts(string productName)
+        {
+            SiparisModel siparis = new SiparisModel();
+            if (!String.IsNullOrEmpty(productName))
+            {
+                siparis.products = _context.ProductModels.Where(x => x.ProductName.Contains(productName)).ToList();
+            }
+            else
+            {
+                siparis.products = new List<ProductModel>();
+
+            }
+            return siparis;
+        }
+
+
+    }
+}
---
+++ order_and_sales_management_ver1/Models/SiparisModel.cs
@@ -0,0 +1,40 @@
+﻿namespace Order_And_Sales_Management_ver1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [NotMapped]
+    public partial class SiparisModel
+    {
+        public SiparisModel()
+        {
+            OrderDetailsModels = new HashSet<OrderDetailsModel>();
+        }
+
+        [Key]
+        [ForeignKey("OrderDetailsModels")]
+        public int orderID { get; set; }
+        [Display(Name = "Sipariş Tarihi")]
+        public DateTime orderDate { get; set; }
+        public bool recStatus { get; set; }
+        [ForeignKey("EmployeesModel")]
+        [Display(Name = "Sipariş Veren")]
+        public int orderOwner_personelID { get; set; }
+        public virtual EmployeesModel orderOwnerEmployeeModel { get; set; }
+        public virtual ICollection<OrderDetailsModel> OrderDetailsModels { get; set; }
+        [ForeignKey("StockLocationModel")]
+        [Display(Name = "Sipariş Lokasyonu")]
+        public int orderLocationID { get; set; }
+        public virtual StockLocationModel orderLocation { get; set; }
+        [Display(Name = "Ürün Adı")]
+        public string productName { get; set; }
+        public int productID { get; set; }
+        public virtual ICollection<ProductModel> products { get; set; }
+        [Display(Name = "Üretim Lot No")]
+        public string productionLotID { get; set; }
+        [Display(Name = "Üretim Miktarı")]
+        public double productAmount { get; set; }
+    }
+}
