c88a4b5adccdc5d89d3e668caf2c94b304a8bd64
tamfatura <tamfatura@tamfatura.com.tr>
2019-09-30 11:13:35 +0300

    DisplaySales içine müşteri geri çağır eklendi.

--- order_and_sales_management_ver1/Controllers/DisplaySalesController.cs
+++ order_and_sales_management_ver1/Controllers/DisplaySalesController.cs
@@ -13,8 +13,12 @@ using MySql.Data.MySqlClient;
 
 namespace Order_And_Sales_Management_ver1.Controllers
 {
+
+
+
     public class DbInterface
     {
+
      //   private string connectionString = "Data Source=GULSEVENSRV\\MySqlEXPRESS;Initial Catalog=GULSEVEN;User ID=sa;Password=QAZwsx135";
 
         public MySqlConnection connect()
@@ -27,7 +31,25 @@ namespace Order_And_Sales_Management_ver1.Controllers
     }
     public class DisplaySalesController : Controller    {
 
-        public  List<DisplaySales> GetDisplaySales()
+        public const string SelectLastCustomerWhoDidPayment = "select max(salesID) as salesID from salesmodels where typeOfCollection>0 and saleDate = @saleDate "; /* Get maximum sales id which is paid on given date */
+        public const string UpdateLastCustomerAsNotPaid = "update salesmodels set typeOfCollection = 0 where saleDate=@saleDate and salesID=@salesID";
+        public const string SelectCustomersWhichSentToCashier =  "select  salesID,sum(amount*productRetailPrice) as tutar,max(paidAmount) as paidTutar " +
+                                                                                                                    "from SalesModels left outer join EmployeesModels  on (SalesModels.personelID = EmployeesModels.personelID) " +
+                                                                                                                    "left outer join ProductModels on(SalesModels.productID=ProductModels.productID) " +
+                                                                                                                    "where typeOfCollection = 0  and saleDate=@saleDate group by salesID";
+
+        public const string SelectSalesDetail =     "select SalesModels.personelID,salesLineID,CONCAT(persName,' ',persSurname) as employee, productName, amount, (amount*productRetailPrice) as tutar " +
+                                                                                    "from SalesModels left outer join EmployeesModels  on (SalesModels.personelID = EmployeesModels.personelID) " +
+                                                                                    "left outer join ProductModels on(SalesModels.productID=ProductModels.productID) " +
+                                                                                    "where typeOfCollection = 0  and SalesModels.salesID=@salesID "+
+                                                                                    "order by salesLineID";
+        public const string UpdateSalesAsPaid = "Update SalesModels set typeOfCollection=@typeOfCollection " +
+                                                                                   "where typeOfCollection = 0  and salesID=@salesID and saleDate = @saleDate";
+
+        public const string UpdatePaidAmount = "Update SalesModels set paidAmount=@paidTutar " +
+                                                                                    "where salesID=@salesID and saleDate=@salesDate";   
+
+        public List<DisplaySales> GetDisplaySales()
         {
             List<DisplaySales> displaySales = new List<DisplaySales>();
             float floatVal = 0;
@@ -40,10 +62,8 @@ namespace Order_And_Sales_Management_ver1.Controllers
 
                 cnn.Open();
                 MySqlCommand mySqlCommand = cnn.CreateCommand();
-                mySqlCommand.CommandText = "select  salesID,sum(amount*productRetailPrice) as tutar,max(paidAmount) as paidTutar " +
-                                                                       "from SalesModels left outer join EmployeesModels  on (SalesModels.personelID = EmployeesModels.personelID) " +
-                                                                       "left outer join ProductModels on(SalesModels.productID=ProductModels.productID) " +
-                                                                       "where typeOfCollection = 0  group by salesID" ;
+                mySqlCommand.CommandText = SelectCustomersWhichSentToCashier;
+                mySqlCommand.Parameters.AddWithValue("@saleDate", DateTime.Now.ToString("yyyy-MM-dd"));
                 MySqlDataReader reader = mySqlCommand.ExecuteReader();
                 int i = 1;
                 while (reader.Read())
@@ -53,7 +73,15 @@ namespace Order_And_Sales_Management_ver1.Controllers
                     displaySale.salesID = reader["salesID"].ToString();
                     float.TryParse(reader["tutar"].ToString(), out floatVal);
                     displaySale.tutar = floatVal.ToString("N2");
-                    displaySale.paidTutar = floatVal.ToString("N2"); 
+                    float.TryParse(reader["paidTutar"].ToString(), out floatVal);
+                    if (floatVal == 0)
+                    {
+                        displaySale.paidTutar = displaySale.tutar;
+                    }
+                    else
+                    {
+                        displaySale.paidTutar = floatVal.ToString("N2");
+                    }
                     displaySales.Add(displaySale);
                     i = i + 1;
                 }
@@ -82,11 +110,7 @@ namespace Order_And_Sales_Management_ver1.Controllers
             {
                 cnn.Open();
                 MySqlCommand mySqlCommand = cnn.CreateCommand();
-                mySqlCommand.CommandText = "select SalesModels.personelID,salesLineID,CONCAT(persName,' ',persSurname) as employee, productName, amount, (amount*productRetailPrice) as tutar " +
-                                                                       "from SalesModels left outer join EmployeesModels  on (SalesModels.personelID = EmployeesModels.personelID) " +
-                                                                       "left outer join ProductModels on(SalesModels.productID=ProductModels.productID) " +
-                                                                       "where typeOfCollection = 0  and SalesModels.salesID=@salesID "+
-                                                                       "order by salesLineID" ;
+                mySqlCommand.CommandText = SelectSalesDetail;
                 mySqlCommand.Parameters.AddWithValue("@salesID", salesID);
                 MySqlDataReader reader = mySqlCommand.ExecuteReader();
                 while (reader.Read())
@@ -139,8 +163,7 @@ namespace Order_And_Sales_Management_ver1.Controllers
             {
                 cnn.Open();
                 MySqlCommand mySqlCommand = cnn.CreateCommand();
-                mySqlCommand.CommandText = "Update SalesModels set typeOfCollection=@typeOfCollection " +
-                                                                       "where typeOfCollection = 0  and salesID=@salesID and saleDate = @saleDate";   /* Date eklenmeli */
+                mySqlCommand.CommandText = UpdateSalesAsPaid;
                 mySqlCommand.Parameters.AddWithValue("@saleDate", DateTime.Now.ToString("yyyy-MM-dd"));
                 mySqlCommand.Parameters.AddWithValue("@typeOfCollection", typeOfCollection);
                 mySqlCommand.Parameters.AddWithValue("@salesID", salesID);
@@ -157,6 +180,43 @@ namespace Order_And_Sales_Management_ver1.Controllers
 
         }
 
+        public ActionResult updateLastCustomerAsNotPaid()
+        {
+            MySqlConnection cnn;
+            string modelError = "";
+            string salesID="0";
+            DbInterface dbInterface = new DbInterface();
+            cnn = dbInterface.connect();
+
+            try
+            {
+                cnn.Open();
+                MySqlCommand mySqlCommand = cnn.CreateCommand();
+                mySqlCommand.CommandText = SelectLastCustomerWhoDidPayment;
+                mySqlCommand.Parameters.AddWithValue("@saleDate", DateTime.Now.ToString("yyyy-MM-dd"));
+                MySqlDataReader reader = mySqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    salesID = reader["salesID"].ToString();
+                }
+                reader.Close();
+                mySqlCommand.Dispose();
+                mySqlCommand = cnn.CreateCommand();
+                mySqlCommand.CommandText = UpdateLastCustomerAsNotPaid;
+                mySqlCommand.Parameters.AddWithValue("@saleDate", DateTime.Now.ToString("yyyy-MM-dd"));
+                mySqlCommand.Parameters.AddWithValue("@salesID", salesID);
+                mySqlCommand.ExecuteNonQuery();
+                mySqlCommand.Dispose();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("VeriTabaniGuncelleme", "Veri tabanı Güncelleme Hatası");
+                modelError = "Veri tabanı Güncelleme Hatası";
+            }
+            if (cnn != null) cnn.Close();
+            return RedirectToAction("Index", "DisplaySales", new { error = modelError });
+        }
+
         public void updatePaidAmount(string salesID, string paidAmount)
         {
             MySqlConnection cnn;
@@ -168,8 +228,7 @@ namespace Order_And_Sales_Management_ver1.Controllers
             {
                 cnn.Open();
                 MySqlCommand mySqlCommand = cnn.CreateCommand();
-                mySqlCommand.CommandText = "Update SalesModels set paidAmount=@paidTutar " +
-                                                                       "where salesID=@salesID and saleDate=@salesDate";   /* Date eklenmeli */
+                mySqlCommand.CommandText = UpdatePaidAmount;
                 mySqlCommand.Parameters.AddWithValue("@salesDate", DateTime.Now.ToString("yyyy-MM-dd"));
                 mySqlCommand.Parameters.AddWithValue("@typeOfCollection", 0);
                 int intSalesID = 0;
@@ -190,6 +249,5 @@ namespace Order_And_Sales_Management_ver1.Controllers
         }
 
     }
-
    
 }
